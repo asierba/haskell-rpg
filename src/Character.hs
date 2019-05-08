5f3b46b7fbb3b53b72fd848b9f257648259ba1a5
Asier Barrenetxea <asier.b@thoughtworks.com>
2019-05-08 23:03:07 +0200

    Chracter doesnt get a negative health

--- src/Character.hs
+++ src/Character.hs
@@ -21,5 +21,11 @@ create = Character { health= Health 1000, level=Level 1, isAlive=True }
 damage ::  Damage -> Character -> Character
 damage d c@Character{health=h} = c{health=newHealth, isAlive=newAlive}
   where newHealth = subtractDamageHealth d h
-        subtractDamageHealth (Damage da) (Health ha) = Health (ha-da)
         newAlive = newHealth > 0
+
+subtractDamageHealth :: Damage -> Health -> Health
+subtractDamageHealth (Damage d) (Health h)
+  | newHealth < 0   = Health 0
+  | otherwise = Health newHealth
+  where newHealth = h-d
+
