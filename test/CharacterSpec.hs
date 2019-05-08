e47769ec6d59e7bbd36e910590db3feda21ef09b
Asier Barrenetxea <asier.b@thoughtworks.com>
2019-05-08 22:27:02 +0200

    Introduce types for Health and Level

--- test/CharacterSpec.hs
+++ test/CharacterSpec.hs
@@ -2,14 +2,15 @@ module CharacterSpec where
 
 import Test.Hspec
 import qualified Character
+import Character (Health(..), Level(..))
 
 spec :: Spec
 spec = do
   describe "Character" $ do
     describe "create" $ do
       it "creates a character with health at 1000" $ do
-        Character.health Character.create `shouldBe` 1000
+        Character.health Character.create `shouldBe` Health 1000
       it "creates a character with a level 1" $ do
-        Character.level Character.create `shouldBe` 1
+        Character.level Character.create `shouldBe` Level 1
       it "creates a character alive" $ do
         Character.isAlive Character.create `shouldBe` True
