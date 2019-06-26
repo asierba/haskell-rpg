8e35f0265444d74f51bafc22e15140cb5fbb78c3
Tom Oram <me@tomoram.io>
2019-06-26 20:49:27 +0100

    Players can only damage other players

--- src/Character.hs
+++ src/Character.hs
@@ -1,26 +1,43 @@
 {-# LANGUAGE GeneralisedNewtypeDeriving #-}
-module Character ( create
-                 , health
-                 , level
-                 , isAlive
-                 , damage
-                 , heal
-                 , Health(..)
-                 , Level(..)
-                 , Damage(..)
-                 ) where
+{-# LANGUAGE OverloadedStrings #-}
 
-data Character = Character { health :: Health, level :: Level, isAlive :: Bool }
+module Character
+  ( create
+  , name
+  , health
+  , level
+  , isAlive
+  , damage
+  , heal
+  , Name(..)
+  , Health(..)
+  , Level(..)
+  , Damage(..)
+  ) where
 
+import Data.Text
+
+data Character =
+  Character { name :: Name
+            , health :: Health
+            , level :: Level
+            , isAlive :: Bool
+            }
+  deriving (Eq, Show)
+
+newtype Name = Name Text deriving (Show, Eq)
 newtype Health = Health Int deriving (Show, Eq, Ord, Num)
 newtype Level = Level Int deriving (Show, Eq)
 newtype Damage = Damage Int deriving (Show, Eq, Num)
 
-create :: Character
-create = Character { health= Health 1000, level=Level 1, isAlive=True }
+create :: Name -> Character
+create n = Character { name = n, health = Health 1000, level=Level 1, isAlive=True }
 
-damage ::  Damage -> Character -> Character
-damage d c@Character{health=h} = c{health=newHealth, isAlive=newAlive}
+damage :: Character -> Damage -> Character -> Character
+damage from d c@Character{health=h} =
+  if name from /= name c
+    then c{health=newHealth, isAlive=newAlive}
+    else c
   where newHealth = subtractDamageHealth d h
         newAlive = newHealth > 0
 
--- test/CharacterSpec.hs
+++ test/CharacterSpec.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module CharacterSpec where
 
 import Test.Hspec
@@ -7,29 +9,38 @@ import Character (Health(..), Level(..), Damage(..))
 spec :: Spec
 spec = do
   describe "Character" $ do
-    let deadCharacter = damage (Damage 1000) create
+    let player1 = create $ Name "Player 1"
+    let player2 = create $ Name "Player 2"
+    let deadCharacter = damage player1 (Damage 1000) (create $ Name "Dead Player")
     describe "create" $ do
+      it "creates a character with its given name" $ do
+        name player1 `shouldBe` Name "Player 1"
       it "creates a character with health at 1000" $ do
-        health create `shouldBe` Health 1000
+        health player1 `shouldBe` Health 1000
       it "creates a character with a level 1" $ do
-        level create `shouldBe` Level 1
+        level player1 `shouldBe` Level 1
       it "creates a character alive" $ do
-        isAlive create `shouldBe` True
+        isAlive player1 `shouldBe` True
     describe "damage" $ do
       it "is subtracted from Health" $ do
-        health (damage (Damage 10) create) `shouldBe` Health 990
+        health (damage player2 (Damage 10) player1) `shouldBe` Health 990
       it "character dies when Health is 0" $ do
         isAlive deadCharacter `shouldBe` False
       it "character dies when Health is 0" $ do
-        health (damage (Damage 1001) create) `shouldBe` Health 0
+        health (damage player2 (Damage 1001) player1) `shouldBe` Health 0
+      it "cannot damage itself" $ do
+        damage player1 (Damage 10) player1 `shouldBe` player1
+      it "cannot damage a player with the same name" $ do
+        let otherPlayer1 = damage player2 (Damage 5) (create $ Name "Player 1")
+        damage otherPlayer1 (Damage 10) player1 `shouldBe` player1
     describe "heal" $ do
       it "is add to Health" $ do
-        let character = (damage (Damage 10) create)
+        let character = (damage player2 (Damage 10) player1)
         health (heal (Health 5) character) `shouldBe` Health 995
       it "dead character cannot be healed" $ do
         health (heal (Health 5) deadCharacter) `shouldBe` Health 0
         isAlive (heal (Health 5) deadCharacter) `shouldBe` False
       it "is add to Health" $ do
-        let character = (damage (Damage 10) create)
+        let character = (damage player2 (Damage 10) player1)
         health (heal (Health 20) character) `shouldBe` Health 1000
 
